c2e5d2b7778a06080a91df2d45ae84bc8087f682
dingdongzhao <ddzyan@163.com>
2022-06-19 15:24:27 +0800

    fix(优化属性声明): 🐞

--- index.d.ts
+++ index.d.ts
@@ -3,15 +3,17 @@ export * from './dist/index';
 declare module '@midwayjs/core/dist/interface' {
   interface MidwayConfig {
     bull?: {
-      default: {
-        redis: {
-          port: number;
-          host: string;
-          username?: string;
-          password?: string;
-          db: number;
-        };
-      };
+      [propName: string]:
+        | {
+            redis: {
+              port: number;
+              host: string;
+              username?: string;
+              password?: string;
+              db: number;
+            };
+          }
+        | string;
       prefix: string;
     };
   }
